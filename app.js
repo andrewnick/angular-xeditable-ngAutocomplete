4d751bdca24e72d6b2588acea672572be1aba42a
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2016-12-10 12:21:06 +1300

    remove bootstrap styling and remove error messages

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = angular.module("app", ["xeditable", "ngTagsInput", 'ngAutocomplete']);
 
-app.run(function(editableOptions) {
-  editableOptions.theme = 'bs3';
-});
-
 app.controller('AutoCompleteCtrl', function($scope, $http) {
   $scope.vm = {
     address: {}
